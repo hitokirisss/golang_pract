1b42ee067c9e519a5b30ce69db43825be6647701
Oleg <evgeev.oleg@bk.ru>
2024-04-22 19:32:33 +0300

    init

--- main.go
+++ main.go
@@ -1,19 +1,12 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	cache := make(map[int]int)
-	var nums [10]int
-	for i := 0; i < 10; i++ {
-		fmt.Scan(&nums[i])
-	}
-	for i := 0; i < 10; i++ {
-		if result, ok := cache[nums[i]]; ok {
-			fmt.Print(result)
-		} else {
-
-		}
-	}
-
-}
+// func main() {
+// 	cache := make(map[int]int)
+// 	var nums [10]int
+// 	for i := 0; i < 10; i++ {
+// 		fmt.Scan(&nums[i])
+// 	}
+// 	}
+// }
